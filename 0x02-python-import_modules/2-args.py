7a16a4dd7583068db179e22459acf7a29c5ab321
nehpets <nehpets0701@gmail.com>
2020-12-10 17:37:38 +0000

    added main check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    if len(sys.argv) == 2:
-        print("1 argument:")
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
     for x in range(1, len(sys.argv)):
         print("{}: {}".format(x, sys.argv[x]))

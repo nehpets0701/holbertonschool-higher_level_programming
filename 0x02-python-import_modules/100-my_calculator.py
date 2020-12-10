a68eee00b0dd2f15988c8c0fbba58c22fce909dc
nehpets <nehpets0701@gmail.com>
2020-12-10 18:15:26 +0000

    import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 import sys
 
 if len(argv) != 3:
@@ -7,13 +7,13 @@ if len(argv) != 3:
     sys.exit(1)
 
 if sys.argv[2] == '+':
-    print(calculator_1.add(sys.argv[1], sys.argv[3]))
+    print(add(sys.argv[1], sys.argv[3]))
 elif sys.argv[2] == '-':
-    print(calculator_1.sub(sys.argv[1], sys.argv[3]))
+    print(sub(sys.argv[1], sys.argv[3]))
 elif sys.argv[2] == '*':
-    print(calculator_1.mul(sys.argv[1], sys.argv[3]))
+    print(mul(sys.argv[1], sys.argv[3]))
 elif sys.argv[2] == '/':
-    print(calculator_1.div(sys.argv[1], sys.argv[3]))
+    print(div(sys.argv[1], sys.argv[3]))
 else:
     print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1) 
+    sys.exit(1)

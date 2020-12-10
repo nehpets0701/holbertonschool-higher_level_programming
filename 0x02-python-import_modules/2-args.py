13e63116015bfdbb231bd10cca08fd2d81aeb040
nehpets <nehpets0701@gmail.com>
2020-12-10 17:27:00 +0000

    added missing line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,6 @@ import sys
 if len(sys.argv) == 1:
     print("0 arguments.")
 else:
+    print("{} arguments".format(len(sys.argv) - 1))
     for x in range(1, len(sys.argv)):
         print("{}: {}".format(x, sys.argv[x]))

e22554245c82bf82c99942084b52db72ee474fb4
nehpets <nehpets0701@gmail.com>
2020-12-10 19:20:04 +0000

    changed 3 to 4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(argv) != 3:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 

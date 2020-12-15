f970b112cf336d5537c7d7e15bd48b063111e2d2
nehpets <nehpets0701@gmail.com>
2020-12-15 19:28:44 +0000

    pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	for x in matrix:
-		for y in range(len[x]):
-			print("{}".format(x[y]), end="")
-			if y <= len(inner):
-				print(" ", end="")
+    for x in matrix:
+        for y in range(len[x]):
+            print("{}".format(x[y]), end="")
+            if y <= len(x):
+                print(" ", end="")
+    print()

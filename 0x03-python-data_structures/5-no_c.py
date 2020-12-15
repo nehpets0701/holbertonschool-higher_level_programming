a1d75fd895a2a355309815f479e8df6094400df9
nehpets <nehpets0701@gmail.com>
2020-12-15 19:43:34 +0000

    new

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for x in range(len(my_string)):
-        if my_string[x] == 'c' or my_string[x] == 'C':
-            my_string[x] = ""
+    new = ""
+    for i in range(0, len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new += my_string[i]
+    return(new)

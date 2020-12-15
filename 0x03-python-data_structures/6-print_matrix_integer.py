2078a51d497c4a0c4128e5c3bb0d70fa03b98923
nehpets <nehpets0701@gmail.com>
2020-12-15 18:09:38 +0000

    reboot

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+	for x in matrix:
+		for y in range(len[x]):
+			print("{}".format(x[y]), end="")
+			if y <= len(inner):
+				print(" ", end="")

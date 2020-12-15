2291d03f92c6e17c2a451c45f9682495bfa0c502
nehpets <nehpets0701@gmail.com>
2020-12-15 19:31:26 +0000

    brackets

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(len(x)):
-            print("{}".format(x[y]), end="")
+            print("{:d}".format(x[y]), end="")
             if y <= len(x):
                 print(" ", end="")
     print()

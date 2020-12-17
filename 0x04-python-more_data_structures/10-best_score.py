3e63371c23492e2dc11a67c246143ebb8473eefa
nehpets <nehpets0701@gmail.com>
2020-12-17 20:06:04 +0000

    none check

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best = 0
 
-    if len(a_dictionary) == 0:
+    if len(a_dictionary) == 0 or a_dictionary is None:
         return None
 
     for x in a_dictionary:

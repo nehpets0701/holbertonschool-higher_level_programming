9856da8d513903c844896a8eed661e941bc5a250
nehpets <nehpets0701@gmail.com>
2020-12-17 07:25:41 +0000

    pushing 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+    new = my_list[:]
+    for x in range(len(new)):
+        if new[x] == search:
+            new[x] = replace
+    return new

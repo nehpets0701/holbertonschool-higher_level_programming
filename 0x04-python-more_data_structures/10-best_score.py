1fa8351cbef9a20c6571e2007f03428a828f7ed9
nehpets <nehpets0701@gmail.com>
2020-12-17 20:11:56 +0000

    could be negative?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best = 0
-
     if len(a_dictionary) == 0 or a_dictionary is None:
         return None
 
+    best = a_dictionary.get(0)
     for x in a_dictionary:
         if a_dictionary.get(x) > best:
             best = a_dictionary.get(x)

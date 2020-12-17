74d1b6a9c875f1ab5430359809862fcc6838a2b4
nehpets <nehpets0701@gmail.com>
2020-12-17 20:49:27 +0000

    different combination

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     bestKey = list(a_dictionary.keys())[0]
     for x in a_dictionary:

f7cdd67720bbfe463730772a10a2dce513caef9c
nehpets <nehpets0701@gmail.com>
2020-12-17 07:31:33 +0000

    pushing 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    
+    sum = 0
+    for x in range(len(my_list)):
+        if my_list[x] not in my_list[:x]:
+            sum += my_list[x]
+    return sum

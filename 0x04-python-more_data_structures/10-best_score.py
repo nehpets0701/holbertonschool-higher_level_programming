696ceba3418ed9ea95922365f3adc5345443dfb0
nehpets <nehpets0701@gmail.com>
2020-12-17 20:12:38 +0000

    0 again

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if len(a_dictionary) == 0 or a_dictionary is None:
         return None
 
-    best = a_dictionary.get(0)
+    best = 0
     for x in a_dictionary:
         if a_dictionary.get(x) > best:
             best = a_dictionary.get(x)

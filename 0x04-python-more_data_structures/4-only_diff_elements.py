06f364a100463afb9338cba7c2b795517f34d945
nehpets <nehpets0701@gmail.com>
2020-12-17 17:36:11 +0000

    fixing 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,8 +3,8 @@ def only_diff_elements(set_1, set_2):
     diff = []
     for x in set_1:
         if x not in set_2:
-            diff.append(element)
+            diff.append(x)
     for x in set_2:
         if x not in set_1:
-            diff.append(element)
+            diff.append(x)
     return diff

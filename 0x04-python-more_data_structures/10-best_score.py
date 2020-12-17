bd719c65e7c1ca70aa706195ff0a6c6028936b82
nehpets <nehpets0701@gmail.com>
2020-12-17 19:52:41 +0000

    pushing 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    
+    check = 0
+
+    if len(a_dictionary) == 0:
+       return None
+
+    for x in a_dictionary:
+        if a_dictionary.get(x) > check:
+            check = a_dictionary.get(x)
+    return check

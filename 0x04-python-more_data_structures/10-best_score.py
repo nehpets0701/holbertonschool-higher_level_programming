01f57c3a964b21326bfb58ad31d7b208f82eef0d
nehpets <nehpets0701@gmail.com>
2020-12-17 20:43:45 +0000

    getting keys

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if len(a_dictionary) == 0 or a_dictionary is None:
         return None
-    bestKey = a_dictionary[0]
+    bestKey = a_dictionary.keys()[0]
     for x in a_dictionary:
         if a_dictionary[x] > a_dictionary[bestKey]:
             bestKey = x

232ffb59a906e95bcf1e7ffae361cc50d64fce88
nehpets <nehpets0701@gmail.com>
2020-12-17 19:42:09 +0000

    making copy

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for x in range(len(a_dictionary)):
-        a_dictionary[x] *= 2
-    return a_dictionary
+    new = {}
+    for x in a_dictionary:
+        new[x] = a_dictionary[x] * 2
+    return new

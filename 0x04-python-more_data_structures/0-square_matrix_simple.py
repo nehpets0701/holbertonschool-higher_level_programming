0d348548cc979f775615e5ccf2651adfd578ef71
nehpets <nehpets0701@gmail.com>
2020-12-17 06:29:44 +0000

    pushing 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
+    new = []
+    for x in range(len(matrix)):
+        new.append(list(map(lambda x: x * x, matrix[x])))
+    return new

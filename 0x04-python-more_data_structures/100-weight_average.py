6e821b5a041a8006523133af4f0168c14dca564c
nehpets <nehpets0701@gmail.com>
2020-12-18 03:37:01 +0000

    pushing 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,8 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     sum = 0
-    weight = 0
+    num = 0
     for x in my_list:
         sum += x[0] * x[1]
-        weight += x[1]
-    return sum / weight
+        num += x[1]
+    return sum / num

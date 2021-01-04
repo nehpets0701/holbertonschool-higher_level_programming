2996a61d7c11bbe0046da7506f749994e1c102e4
nehpets <nehpets0701@gmail.com>
2021-01-04 18:27:08 +0000

    continue instead of break

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print(my_list[y], end="")
             printed = printed + 1
         except(ValueError, TypeError):
-            break
+            continue
     print("")
     return printed

2cc2cb374ce1fb68c6fed9d06802b0b7a2563a4d
nehpets <nehpets0701@gmail.com>
2021-01-04 17:48:09 +0000

    added count variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    printed = 0
     for y in range(x):
         try:
             print("{:d}".format(y), end="")
+            printed++
         except:
             break
     print("")

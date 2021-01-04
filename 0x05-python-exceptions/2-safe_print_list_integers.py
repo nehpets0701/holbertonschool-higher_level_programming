6ac6b0dd43ba917a11f9fe5925bde17064afcc48
nehpets <nehpets0701@gmail.com>
2021-01-04 18:25:32 +0000

    typeerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print(my_list[y], end="")
             printed = printed + 1
-        except(ValueError):
+        except(ValueError, TypeError):
             break
     print("")
     return printed

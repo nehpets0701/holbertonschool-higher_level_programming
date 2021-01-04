6e36ae743146754680d96b53c4bf1b7f22389f8b
nehpets <nehpets0701@gmail.com>
2021-01-04 20:05:00 +0000

    space

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except(TypeError, ValueError) as err:
-        sys.stderr.write("Exception:" + str(err) + '\n')
+        sys.stderr.write("Exception: " + str(err) + '\n')
         return False

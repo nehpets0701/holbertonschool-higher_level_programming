9d086d65f846c69137195cafd4af86de31a8231b
nehpets <nehpets0701@gmail.com>
2021-01-04 18:23:18 +0000

    value

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    for y in range(x):
+        try:
+            print(my_list[y], end="")
+            printed = printed + 1
+        except(ValueError):
+            break
+    print("")
+    return printed

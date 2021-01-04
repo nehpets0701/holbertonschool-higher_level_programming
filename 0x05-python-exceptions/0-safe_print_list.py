a9f388afb4927752aaee2e7696bb4112a9b7d85b
nehpets <nehpets0701@gmail.com>
2021-01-04 17:44:39 +0000

    index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     for y in my_list:
         try:
             print("{:d}".format(y), end="")
-        except IndexError:
+        except:
             break
     print("")
     return y

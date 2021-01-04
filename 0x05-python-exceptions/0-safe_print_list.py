fe08f15b6b974f61acf61c69a33de316f3adf90b
nehpets <nehpets0701@gmail.com>
2021-01-04 17:29:41 +0000

    initial

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/pin/pythpn3
+def safe_print_list(my_list=[], x=0):
+	for y in my_list:
+        try:
+			print("{:d}".format(y), end="")
+		except IndexError:
+			break
+	print()
+	return y + 1

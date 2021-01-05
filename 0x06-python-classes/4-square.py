0595263776e849ea34b8ee1fce9c2bfdcac6d76f
nehpets <nehpets0701@gmail.com>
2021-01-05 21:50:45 +0000

    triple quotes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"class comment outside"
+"""class comment outside"""
 
 
 class Square:
-    "Square class"
+    """Square class"""
     def __init__(self, size=0):
-        "function to initialize size"
+        """function to initialize size"""
         self.__size = size
 
     def area(self):
-        "function to calculate area"
+        """function to calculate area"""
         return (self.__size * self.__size)
 
     @property
     def size(self):
-        "function to get size"
+        """function to get size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        "function to set size"
+        """function to set size"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:

64283b9edc3fa6df370b1a9e81a4a21fc3bc746d
nehpets <nehpets0701@gmail.com>
2021-01-19 23:51:18 +0000

    comments

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""compares instances"""
+
+
 def is_kind_of_class(obj, a_class):
+    """true/false"""
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""inherits from"""
+
+
 def inherits_from(obj, a_class):
+    """true/false"""
     if type(obj) is a_class:
         return False
     return issubclass(type(obj), a_class)

93094f1a128d1e414102b97de18c6ac53e44f948
nehpets <nehpets0701@gmail.com>
2021-01-20 18:02:42 +0000

    pushing 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+import json
+
+
 def load_from_json_file(filename):
-    
+    with open(filename) as file:
+        str = file.read()
+        json.load(str)
+        file.close()

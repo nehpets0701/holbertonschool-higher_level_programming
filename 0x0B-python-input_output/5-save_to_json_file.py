b9cd96dccebd74bef66f72921d360ffe63fd49bd
nehpets <nehpets0701@gmail.com>
2021-01-21 00:24:21 +0000

    comments

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import json
+"""function"""
 
 
 def save_to_json_file(my_obj, filename):
+    """function"""
     with open(filename) as file:
         file.write(json.dumps(my_obj))
         file.close()

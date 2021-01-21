9b05f0d693f4f4f98a6e393030428b641bb5e516
nehpets <nehpets0701@gmail.com>
2021-01-21 01:02:46 +0000

    pushing 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,11 @@
 class Student:
     """student class"""
     def __init__(self, first_name, last_name, age):
-        """init function"""
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """to json function"""
+        """return dict"""
+        return vars(self)

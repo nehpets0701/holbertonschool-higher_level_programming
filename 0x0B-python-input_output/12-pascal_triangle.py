a00e7a4ee4c65c29f08a640d88e6783af54166d4
nehpets <nehpets0701@gmail.com>
2021-01-21 00:51:59 +0000

    pushing 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""function"""
+
+
 def pascal_triangle(n):
-    
+    """function"""
+    row = [1]
+    y = [0]
+    for x in range(n):
+        print(row)
+        row = [left+right for left, right in zip(row + y, y + row)]
+    return n >= 1

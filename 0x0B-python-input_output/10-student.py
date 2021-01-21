b8a2b007961fe00853b7e17799e9127ec1a9b040
nehpets <nehpets0701@gmail.com>
2021-01-21 00:39:23 +0000

    pushing 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json"""
+        if attrs is None or type(attrs) is not list:
+            return vars(self)
+        else:
+            new = {}
+            for x in attrs:
+                if type(x) is str:
+                    try:
+                        new[x] = self.__dict__[x]
+                    except:
+                        pass
+            return new

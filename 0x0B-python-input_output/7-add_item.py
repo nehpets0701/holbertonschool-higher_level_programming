1f5da52155aa3a0139b533a4915326a5deb3ec0b
nehpets <nehpets0701@gmail.com>
2021-01-21 00:33:19 +0000

    fixing 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""script"""
+from sys import argv
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+file = "add_item.json"
+try:
+    new = load(file)
+except:
+    with open(file, 'w'):
+        new = []
+new += argv[1:]
+save(new, file)

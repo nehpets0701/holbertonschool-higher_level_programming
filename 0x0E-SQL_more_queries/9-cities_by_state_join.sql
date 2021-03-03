5218ba0e05764bde70b8f722f43a132c84536aff
nehpets <nehpets0701@gmail.com>
2021-03-03 03:29:51 +0000

    semicolon

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 --join
-SELECT cities.id, cities.name, states.name
-FROM cities LEFT JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+LEFT JOIN states ON cities.state_id = states.id;

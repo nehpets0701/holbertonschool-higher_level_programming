a146fd306839d6c305fb47a885d8823a2dee9fc5
nehpets <nehpets0701@gmail.com>
2021-03-22 21:39:53 +0000

    all

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  let temp;
+  for (let i = 0; i < process.argv.length; i++) {
+    for (let j = i + 1; j < process.argv.length; j++) {
+      if (parseInt(process.argv[i]) > parseInt(process.argv[j])) {
+        temp = process.argv[i];
+        process.argv[i] = process.argv[j];
+        process.argv[j] = temp;
+      }
+    }
+  }
+  console.log(process.argv[process.argv.length - 2]);
+}

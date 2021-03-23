d56dbd1b2eeafca673d61af61a6d58c2e7f0406f
nehpets <nehpets0701@gmail.com>
2021-03-23 22:36:01 +0000

    all

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const S = require('./5-square');
+module.exports = class Square extends S {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};

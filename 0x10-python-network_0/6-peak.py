4f313125404e6cdbccbd38646d489f02c99ae010
nehpets <nehpets0701@gmail.com>
2021-04-08 19:52:03 +0000

    testing

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    for x in (list_of_integers):
+        if x == len(list_of_integers) - 1:
+            if list_of_integers[x] >= list_of_integers[x-1]:
+                return list_of_integers[x]
+        elif x == 0:
+            if list_of_integers[x] >= list_of_integers[x+1]:
+                return list_of_integers[x]
+        elif list_of_integers[x] >= list_of_integers[x-1] and list_of_integers[x] >= list_of_integers[x+1]:
+            return list_of_integers[x]

edf439aa88ad36408258fd49b8a855e05435ed5d
nehpets <nehpets0701@gmail.com>
2021-04-08 20:10:14 +0000

    checker

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-    for x in (list_of_integers):
+    for x in range(len(list_of_integers)):
         if x == len(list_of_integers) - 1:
             if list_of_integers[x] >= list_of_integers[x-1]:
-                return list_of_integers[x]
+                if list_of_integers[x] >= 0:
+                    return list_of_integers[x]
         elif x == 0:
             if list_of_integers[x] >= list_of_integers[x+1]:
-                return list_of_integers[x]
+                if list_of_integers[x] >= 0:
+                    return list_of_integers[x]
         elif list_of_integers[x] >= list_of_integers[x-1] and list_of_integers[x] >= list_of_integers[x+1]:
-            return list_of_integers[x]
+            if list_of_integers[x] >= 0:
+                return list_of_integers[x]

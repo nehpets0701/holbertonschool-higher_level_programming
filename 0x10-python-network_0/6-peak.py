58fa2208836e6756e2edda6e466b239649b44556
nehpets <nehpets0701@gmail.com>
2021-04-09 00:26:13 +0000

    finished

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""peak problem"""
+
 
 def find_peak(list_of_integers):
-    for x in range(len(list_of_integers)):
-        if x == len(list_of_integers) - 1:
-            if list_of_integers[x] >= list_of_integers[x-1]:
-                if list_of_integers[x] >= 0:
-                    return list_of_integers[x]
-        elif x == 0:
-            if list_of_integers[x] >= list_of_integers[x+1]:
-                if list_of_integers[x] >= 0:
-                    return list_of_integers[x]
-        elif list_of_integers[x] >= list_of_integers[x-1] and list_of_integers[x] >= list_of_integers[x+1]:
-            if list_of_integers[x] >= 0:
-                return list_of_integers[x]
+    """wrapper function"""
+    if list_of_integers == []:
+        return None
+    return(find(list_of_integers, 0, len(list_of_integers) - 1))
+
+def find(numlist, low, high):
+    """real function"""
+    size = high - low
+    mid = low + int(size/2)
+    if low == high or ((numlist[mid - 1] < numlist[mid]) and
+                       (numlist[mid + 1] < numlist[mid])):
+        return numlist[mid]
+    elif numlist[mid + 1] > numlist[mid]:
+        return find(numlist, mid + 1, high)
+    else:
+        return find(numlist, low, mid - 1)
